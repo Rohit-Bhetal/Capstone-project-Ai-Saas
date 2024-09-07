3e13f0a45b55b2248811a5133bc257132f79b934
Rohit Bhetal <96344296+Rohit-Bhetal@users.noreply.github.com>
2024-09-07 23:17:08 +0545

    Folder Struc

---
+++ components/navbar.tsx
@@ -0,0 +1,8 @@
+const Navbar = ()=>{
+    return <div className="flex items-center p-4">
+                <div className="flex w-full justify-end">
+                {/* <UserButton afterSignOutUrl="/" /> */}
+                </div>
+            </div>
+}
+export default Navbar;
